891fb594b16692fa561089d952d94f962b0781ec
Md Shajedul Islam <j.shajid@gmail.com>
2022-05-18 17:42:12 -0500

    Implementation of central configuration server for microservices using Spring Cloud Config

---
+++ src/main/java/com/miu/swa/microserviceapp/cloudconfig/CloudConfigApplication.java
@@ -0,0 +1,18 @@
+package com.miu.swa.microserviceapp.cloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class CloudConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigApplication.class, args);
+        System.out.println("***************************************");
+        System.out.println("***************************************");
+
+    }
+
+}
---
+++ src/test/java/com/miu/swa/microserviceapp/MicroserviceappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miu.swa.microserviceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
